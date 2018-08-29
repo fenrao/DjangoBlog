120216904ebb003e79a6aecb4e8ccefa5eea7748
fenrao <228283691@qq.com>
2018-08-29 10:33:34 +0800

    修改配置

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -99,11 +99,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'djangoblog',
-        'USER': os.environ.get('DJANGO_MYSQL_USER'),
-        'PASSWORD': os.environ.get('DJANGO_MYSQL_PASSWORD'),
-        'HOST': os.environ.get('DJANGO_MYSQL_HOST'),
+        'USER': 'root',
+        'PASSWORD': '123qweasd',
+        'HOST': 'host',
         'PORT': 3306,
-        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 
